5181d7a9c561ad950f8a8fd14064c363901bc84d
Joosep Sisas <joosep.sisas@gmail.com>
2020-07-13 09:34:03 +0300

    Date is not showing time anymore

--- src/util/DateUtil.ts
+++ src/util/DateUtil.ts
@@ -1,5 +1,5 @@
 import moment, {MomentInput} from "moment";
 import 'moment/locale/et';
 
-export const dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a";
+export const dateFormat = "dddd, Do MMMM, YYYY";
 export const date = (date?: MomentInput) => moment(date).locale("et");
