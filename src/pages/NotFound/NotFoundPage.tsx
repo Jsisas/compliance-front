da08130cc9f07dc893e6e824d6ea99d86da6b2a5
Joosep Sisas <joosep.sisas@gmail.com>
2020-07-18 14:37:27 +0300

    #71 | 404-page-call-to-action-should-be-called-back-not-back-home

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
-import {Result} from 'antd';
+import { Result } from 'antd';
 import AlButton from '../../components/_ui/AlButton/AlButton';
-import {RouteComponentProps} from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
 
 export function NotFoundPage(props: RouteComponentProps): JSX.Element {
 	return (
 		<>
 			<Result
-				status="404"
-				title="404"
-				subTitle="Sorry, the page you visited does not exist."
-				extra={<AlButton type="primary" onClick={() => props.history.goBack()}>Back Home</AlButton>}
+				status='404'
+				title='404'
+				subTitle='Sorry, the page you visited does not exist.'
+				extra={
+					<AlButton type='primary' onClick={() => props.history.goBack()}>
+						Go Back
+					</AlButton>
+				}
 			/>
 		</>
 	);
