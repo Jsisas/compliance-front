d1033b61f0858538e245d3e92a6309dc09997b08
Joosep Sisas <joosep.sisas@gmail.com>
2020-09-07 08:15:31 +0300

    upgraded packages

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,5 @@
 const CracoAntDesignPlugin = require("craco-antd");
 
 module.exports = {
-    plugins: [{plugin: CracoAntDesignPlugin}, {
-        plugin: CracoLessPlugin,
-        options: {
-            lessLoaderOptions: {
-                lessOptions: {
-                    javascriptEnabled: true
-                }
-            }
-        }
-    }]
+    plugins: [{plugin: CracoAntDesignPlugin}]
 };
